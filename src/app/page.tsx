de6a3576f7498655040b629383e621d93b9c9a13
Gabriel Martins <martinsgabriel@gmail.com>
2023-05-16 12:20:58 -0300

    feat: first day finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <h1 className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </h1>
   )
 }
